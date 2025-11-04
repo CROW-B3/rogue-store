367bc0879ad61121b586aa840c9fe6ea084d8ad0
Methika1234 <155930844+Methika1234@users.noreply.github.com>
2025-11-04 19:17:18 +0530

    chore: update ESLint configuration and package scripts for improved linting

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import type { NextConfig } from "next";
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
-  cacheComponents: true,
   compress: false,
   poweredByHeader: false,
-  reactCompiler: true,
   trailingSlash: true,
   experimental: {
     serverMinification: false,
